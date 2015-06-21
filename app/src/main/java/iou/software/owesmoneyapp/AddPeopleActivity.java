68d20d0e33f09d6888ea96a0a289cbc09caddcc3
AugustM94 <augustmbius@gmail.com>
2015-06-21 16:41:20 +0200

    Added people activity

--- app/src/main/java/iou/software/owesmoneyapp/AddPeopleActivity.java
+++ app/src/main/java/iou/software/owesmoneyapp/AddPeopleActivity.java
@@ -1,16 +1,22 @@
 package iou.software.owesmoneyapp;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
 
 
 /**
  * Created by August on 19/06/15.
  */
-public class AddPeopleActivity extends ListActivity {
+public class AddPeopleActivity extends Activity {
+
+    EditText mName;
+    EditText mNumber;
+    EditText mAmountPaid;
 
 
 
@@ -19,17 +25,40 @@ public class AddPeopleActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_new_person);
 
+        //Getting the EditText views
+        mName = (EditText)findViewById(R.id.enter_name);
+        mNumber = (EditText)findViewById(R.id.enter_number);
+        mAmountPaid = (EditText)findViewById(R.id.enter_amount_paid);
+
+
+        // Listener for the submit button.
+        final Button submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+            //Getting strings from TextEdits.
+                String name = mName.getText().toString();
+                String number = mNumber.getText().toString();
+                //Getting the amountPaid as string.
+                String amountPaidString = mAmountPaid.getText().toString();
+                //Parsing the string to int.
+                int amountPaid = Integer.parseInt(amountPaidString);
+
+                //Creating the new person with the given data
+                //Person name = new Person(name,number,amountPaid);
+                Person newPerson = new Person(name,number,amountPaid);
+                Log.i("Hello","Person created");
+
+
+            }
+        });
+
+
+
     }
 
 
-    // Listener for the submit button.
-    //final Button submitButton = (Button) findViewById(R.id.submit_button);
-    //submitButton.setOnClickListener(new Button.OnClickListener() {
-    //    public void onClick(View v) {
-            //Do stuff here
-
-    //    }
-    //});
 
 
 
