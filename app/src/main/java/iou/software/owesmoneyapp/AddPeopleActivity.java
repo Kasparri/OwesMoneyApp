c842562588402488ba0f369b515bff74f9273a0b
AugustM94 <augustmbius@gmail.com>
2015-06-21 13:40:12 +0200

    Added people activity

--- app/src/main/java/iou/software/owesmoneyapp/AddPeopleActivity.java
+++ app/src/main/java/iou/software/owesmoneyapp/AddPeopleActivity.java
@@ -1,23 +1,41 @@
 package iou.software.owesmoneyapp;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+
 
 /**
  * Created by August on 19/06/15.
  */
-public class AddPeopleActivity extends ActionBarActivity {
+public class AddPeopleActivity extends ListActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    //    super.onCreate(savedInstanceState);
-    //    setContentView(R.layout.people_list_activity);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_new_person);
 
     }
+
+
+    // Listener for the submit button.
+    //final Button submitButton = (Button) findViewById(R.id.submit_button);
+    //submitButton.setOnClickListener(new Button.OnClickListener() {
+    //    public void onClick(View v) {
+            //Do stuff here
+
+    //    }
+    //});
+
+
+
+
+
+
 
 
 
