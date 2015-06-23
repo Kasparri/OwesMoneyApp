fa1820facf524da6cbe98cc55b4f00745e7c408b
Mads <mads.e.maibohm@gmail.com>
2015-06-23 12:24:36 +0200

    Fixed loading contacts

--- app/src/main/java/iou/software/owesmoneyapp/AddPersonActivity.java
+++ app/src/main/java/iou/software/owesmoneyapp/AddPersonActivity.java
@@ -1,11 +1,10 @@
 package iou.software.owesmoneyapp;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +13,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import java.util.List;
 
@@ -28,7 +26,8 @@ public class AddPersonActivity extends Activity {
     EditText mNumber;
     EditText mAmountPaid;
     List<Person> persons;
-    Cursor cursor;
+    Cursor cursorNames;
+    Cursor cursorNumbers;
 
 
     @Override
@@ -99,13 +98,16 @@ public class AddPersonActivity extends Activity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1) {
             if (resultCode == Activity.RESULT_OK) {
-                String[] projection = new String[]{ContactsContract.Contacts.DISPLAY_NAME};
-                cursor = getContentResolver().query(data.getData(),projection,null,null,null);
-                if (cursor.moveToFirst()) {
-                    //String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                ContentResolver content = getContentResolver();
+                String whereCondition = ContactsContract.Data.MIMETYPE + " = '" +   ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "'";
+                String[] projection = new String[]{ContactsContract.Data.DISPLAY_NAME,ContactsContract.Data.DATA1};
+                String sortOrder = ContactsContract.Data.DISPLAY_NAME;
+                cursorNames = content.query(ContactsContract.Data.CONTENT_URI,projection,whereCondition,null,sortOrder);
+                if (cursorNames.moveToFirst()) {
+                    String phoneNumber = cursorNames.getString(cursorNames.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    String name = cursorNames.getString(cursorNames.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
                     mName.setText(name, TextView.BufferType.EDITABLE);
-                    //mNumber.setText(phoneNumber, TextView.BufferType.EDITABLE);
+                    mNumber.setText(phoneNumber, TextView.BufferType.EDITABLE);
                 }
             }
 
